cb2ff5c5e69c672cb56fb7eec6b9fe403d7d2249
vietduc1441 <vietduc1441@yahoo.com>
2014-10-16 16:20:11 +0200

    finish container

--- VizEditor/widget/module/directives/layout/vizReportContainer.js
+++ VizEditor/widget/module/directives/layout/vizReportContainer.js
@@ -1,26 +1,58 @@
 define(["angular",
         "lib/angular-bootstrap/ui-bootstrap-tpls.min",
         "directives/layout/vizReportLayout",
-        "directives/layout/vizToolBox"
+        "directives/layout/vizToolBox",
+        "services/widgetData"
         ],
     function(angular){
-    angular.module("vizDirectives.layout.vizReportContainer",[
-                                                              "ui.bootstrap",
-                                                              "vizDirectives.layout.vizReportLayout",
-                                                              "vizDirectives.layout.vizToolBox"
+        function getBookData(newVal, oldVal, scope, bookData, sheetData, widgetData, $q){
+            if (newVal===-1) return;
+            bookData.getBookById(scope.bookId).then(function(book){
+                scope.book=book[0];
+                sheetData.getSheets(scope.book.sheetids).then(function(sheets){
+                    scope.book.sheets=sheets;
+                    sheets.forEach(function(sheet){
+                        var q1=$q.defer();
+                        var q2=$q.defer();
+                        sheetData.getSheetData(sheet.id).then(function(sheetData){
+                            sheet.data=sheetData[0].data;
+                            q1.resolve({sheetData:sheetData});
+                        });
+                        widgetData.getWidgets(sheet.widgetids).then(function(widgets){
+                            sheet.widgets=widgets;
+                            q2.resolve({widgets:widgets});
+                        });
+                        $q.all([q1.promise,q2.promise]).then(function(resutls){
+                            sheet.widgets.forEach(function(widget){
+                                widget.data=sheet.data;
+                            });
+                        });
+                    });
+                });
+            });  
+        };
+        angular.module("vizDirectives.layout.vizReportContainer",[
+                                                            "DataManager",
+                                                            "ui.bootstrap",
+                                                            "vizDirectives.layout.vizReportLayout",
+                                                            "vizDirectives.layout.vizToolBox"
                                                             ])
             .directive('vizReportContainer', function () {
                        return {
                                restrict: "EA",
                                scope:{
-                                   book:'='
                                },
-                               controller: function($scope){
+                               controller: ["$scope","bookData","sheetData","widgetData","$q",function($scope, bookData,sheetData,widgetData,$q){
                                    $scope.name='vizReportContainer';
+                                   $scope.book=[];
                                    $scope.bookId=-1;
-                               },
+                                   $scope.$watch("bookId",
+                                                function(newVal,oldVal,scope){
+                                                    getBookData(newVal, oldVal, scope, bookData, sheetData, widgetData, $q);
+                                                });
+                               }],
                                link: function(scope, elm, attrs, ctr){
-                                   scope.bookId=attrs["bookId"];
+                                   scope.bookId=parseInt(attrs["bookId"]);
                                },
                                templateUrl: "widget/module/template/layout/vizReportContainer_tpl.html"
                        };
--- VizEditor/widget/module/vizApp.js
+++ VizEditor/widget/module/vizApp.js
@@ -1,40 +1,15 @@
 define(["angular",
         "vizDirectives",
         "services/ChartData",
-        "services/generator",
-        "services/widgetData"],
+        "services/generator"
+        ],
     function(angular){
     "use strict"
-    var vizApp= angular.module("vizApp",["vizDirectives","DataManager"]);
-    vizApp.controller("vizCtrl",["$scope","bookData","sheetData","widgetData","$q",
-        function($scope, bookData,sheetData,widgetData,$q){
-            var bookId=1;
+    var vizApp= angular.module("vizApp",["vizDirectives"]);
+    vizApp.controller("vizCtrl",["$scope",
+        function($scope){
             this.width="800px";
             this.height="600px";
-            
-            bookData.getBookById(bookId).then(function(book){
-                $scope.book=book[0];
-                sheetData.getSheets($scope.book.sheetids).then(function(sheets){
-                    $scope.book.sheets=sheets;
-                    sheets.forEach(function(sheet){
-                        var q1=$q.defer();
-                        var q2=$q.defer();
-                        sheetData.getSheetData(sheet.id).then(function(sheetData){
-                            sheet.data=sheetData[0].data;
-                            q1.resolve({sheetData:sheetData});
-                        });
-                        widgetData.getWidgets(sheet.widgetids).then(function(widgets){
-                            sheet.widgets=widgets;
-                            q2.resolve({widgets:widgets});
-                        });
-                        $q.all([q1.promise,q2.promise]).then(function(resutls){
-                            sheet.widgets.forEach(function(widget){
-                                widget.data=sheet.data;
-                            });
-                        });
-                    });
-                });
-            }); 
     }]);
     angular.module("Generator")
             .config(["idGeneratorProvider",function(idGenerator){
